ff2568868bde97fcdf347a22de0f8dc6df9c4348
String Iyan <52801719+IyanArce99@users.noreply.github.com>
2022-02-11 15:52:47 +0100

    add chat page

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -8,11 +8,14 @@ import { User } from '../modelos/User';
 })
 export class Tab5Page implements OnInit {
   usuario:User;
-
+  public me: any;
+  public you: any;
   constructor() { }
 
   ngOnInit() {
     this.getUsuario();
+    this.me = "https://lh6.googleusercontent.com/-lr2nyjhhjXw/AAAAAAAAAAI/AAAAAAAARmE/MdtfUmC0M4s/photo.jpg?sz=48";
+    this.you = "https://a11.t26.net/taringa/avatares/9/1/2/F/7/8/Demon_King1/48x48_5C5.jpg";
   }
 
   getUsuario(){
