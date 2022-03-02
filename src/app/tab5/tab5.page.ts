76fe88cb978170cf3fc75b3122b8f16fcc00d4a6
IvanGarcia23 <ivangarciarubio@gmail.com>
2022-03-02 16:48:03 +0100

    recarga componentes

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { User } from '../modelos/User';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { DataService } from '../services/data.service';
@@ -10,36 +10,49 @@ import { ModalMensajesRespuestaPage } from '../pages/modal-mensajes-respuesta/mo
   templateUrl: './tab5.page.html',
   styleUrls: ['./tab5.page.scss'],
 })
-export class Tab5Page implements OnInit {
-  usuario:User;
+export class Tab5Page implements AfterViewInit {
+  usuario: User;
   editarLocationType: boolean = false;
   editarNumberCongressType: boolean = false;
-  usuarioForm:FormGroup
+  usuarioForm: FormGroup
 
-  constructor(private fb:FormBuilder, private _dataService:DataService, private menu: MenuController, private modalController: ModalController) {
-    this.usuarioForm = this.fb.group ({
+  constructor(private fb: FormBuilder, private _dataService: DataService, private menu: MenuController, private modalController: ModalController) {
+    this.usuarioForm = this.fb.group({
       location: '',
       numberCongress: ''
     })
-   }
+  }
 
-  ngOnInit() {
+  ngAfterViewInit(): void {
     this.getUsuario();
   }
 
-  getUsuario(){
-    const datoUsuario = JSON.parse(localStorage.getItem('user-complete'));
-    
-    this.usuario = datoUsuario;
-    this.usuarioForm.get("location").setValue(this.usuario.location);
-    this.usuarioForm.get("numberCongress").setValue(this.usuario.numberCongress);
+  ionViewDidEnter(){
+    this.getUsuario();
   }
 
-  cambiarEditLocation(){
+  getUsuario() {
+    const datoUsuario = JSON.parse(localStorage.getItem('user-complete'));
+    this.usuario = undefined;
+
+    this._dataService.getUsuarioPorId(datoUsuario.id).subscribe(
+      result => {
+        console.log(result.data());
+        this.usuario = result.data();
+
+        this.usuarioForm.get("location").setValue(this.usuario.location);
+        this.usuarioForm.get("numberCongress").setValue(this.usuario.numberCongress);
+      }, error => {
+        console.log(error);
+      }
+    )
+  }
+
+  cambiarEditLocation() {
     this.editarLocationType = !this.editarLocationType;
   }
 
-  editLocation(){
+  editLocation() {
     //limpiamos el localstorage del usuario completo
     localStorage.removeItem('user-complete');
 
@@ -50,19 +63,18 @@ export class Tab5Page implements OnInit {
     this._dataService.editarUsuario(this.usuario.id, this.usuario).then(data => {
       //en caso de que edite correctamente volvemos a crear el localStorage de user-complete
       localStorage.setItem('user-complete', JSON.stringify(this.usuario));
-
-      //recargamos la pagina para que se vean los cambios
-      window.location.reload();
+      this.editarLocationType = false;
+      this.editarNumberCongressType = false;
     }).catch(error => {
       console.log(error);
     })
   }
 
-  cambiarEditNumberCongress(){
+  cambiarEditNumberCongress() {
     this.editarNumberCongressType = !this.editarNumberCongressType;
   }
 
-  editNumberCongress(){
+  editNumberCongress() {
     //limpiamos el localstorage del usuario completo
     localStorage.removeItem('user-complete');
 
@@ -74,8 +86,8 @@ export class Tab5Page implements OnInit {
       //en caso de que edite correctamente volvemos a crear el localStorage de user-complete
       localStorage.setItem('user-complete', JSON.stringify(this.usuario));
 
-      //recargamos la pagina para que se vean los cambios
-      window.location.reload();
+      this.editarLocationType = false;
+      this.editarNumberCongressType = false;
     }).catch(error => {
       console.log(error);
     })
@@ -86,7 +98,7 @@ export class Tab5Page implements OnInit {
     this.menu.open('end');
   }
 
-  async responderMensajes(message:any){
+  async responderMensajes(message: any) {
     const modal = await this.modalController.create({
       component: ModalMensajesRespuestaPage,
       cssClass: 'modalCustomizado',
