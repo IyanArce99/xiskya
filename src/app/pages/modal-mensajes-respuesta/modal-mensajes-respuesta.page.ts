7f62bea14ab51f6c234f7cc774df1653f081f64f
IvanGarcia23 <ivangarciarubio@gmail.com>
2022-03-02 15:09:12 +0100

    cambios mensajes respuesta

--- src/app/pages/modal-mensajes-respuesta/modal-mensajes-respuesta.page.ts
+++ src/app/pages/modal-mensajes-respuesta/modal-mensajes-respuesta.page.ts
@@ -1,9 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { User } from 'firebase/auth';
+import { ModalController, ToastController } from '@ionic/angular';
 import { DataService } from '../../services/data.service';
 import { element } from 'protractor';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { User } from 'src/app/modelos/User';
 
 @Component({
   selector: 'app-modal-mensajes-respuesta',
@@ -12,12 +12,15 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class ModalMensajesRespuestaPage implements OnInit {
   @Input() mensaje:any;
+
   usuarioLogin:User;
   usuarios: Array<User> = [];
   mensajeForm: FormGroup;
+  usuarioMensaje:User;
 
-  constructor(private modalController:ModalController, private _dataService:DataService, private fb:FormBuilder) {
+  constructor(private modalController:ModalController, private _dataService:DataService, private fb:FormBuilder, private toastCtrl: ToastController) {
     this.mensajeForm = this.fb.group ({
+      id: '',
       imagePath : '',
       name: '',
       text: ['', Validators.required],
@@ -26,14 +29,11 @@ export class ModalMensajesRespuestaPage implements OnInit {
   }
 
   ngOnInit() {
-    this.recogerUsuario();
+    this.getUsuarios();
   }
 
-  recogerUsuario(){
-    //recogemos el usuario logueado actualmente
-    this.usuarioLogin = JSON.parse(localStorage.getItem('user-complete'));
-
-    //recorremos los distintos usuarios existentes hasta encontrar al usuario al que vamos a enviar el mensaje
+  getUsuarios() {
+    //recogemos todos los usuarios y comprobamos a cual es al que enviaremos la respuesta
     this._dataService.getUsuarios().subscribe(
       data => {
         this.usuarios = [];
@@ -44,10 +44,12 @@ export class ModalMensajesRespuestaPage implements OnInit {
           })
         });
 
-        this.usuarios.forEach(element=>{
-          console.log(element);
-        })
-        
+        //comprobamos el id del mensaje con el id del usuario
+        this.usuarios.forEach(element => {
+          if(element.id == this.mensaje.id){
+            this.usuarioMensaje = element;
+          }
+        });
       }, error => {
         console.log(error);
       }
@@ -55,8 +57,36 @@ export class ModalMensajesRespuestaPage implements OnInit {
   }
 
   enviarRespuesta(){
-    //pusheamos el mensaje al usuario en cuestión
-    
+    //guardamos el nombre de quien envia el mensaje
+    this.usuarioLogin = JSON.parse(localStorage.getItem('user-complete'));
+
+    //guardamos la fecha del momento exacto del mensaje
+    const fecha:Date = new Date();
+
+    //guardamos los datos del formulario
+    this.mensajeForm.get('id').setValue(this.usuarioLogin.id);
+    this.mensajeForm.get('imagePath').setValue(this.usuarioLogin.imagePath);
+    this.mensajeForm.get('name').setValue(this.usuarioLogin.name);
+    this.mensajeForm.get('fecha').setValue(fecha);
+
+    //pusheamos el mensaje
+    this.usuarioMensaje.messages.push(this.mensajeForm.value);
+
+    //editamos el usuario con el nuevo mensaje
+    this._dataService.editarUsuario(this.usuarioMensaje.id, this.usuarioMensaje).then(data=>{
+      let toast = this.toastCtrl.create({
+        message: 'Respuesta enviada correctamente',
+        duration: 3000,
+        position: 'bottom',
+        color: 'success'
+      }).then((data) => {
+        data.present();
+      })
+
+      this.cerrarModal();
+    }).catch (error => {
+      console.log(error);
+    })
   }
 
   cerrarModal(){
